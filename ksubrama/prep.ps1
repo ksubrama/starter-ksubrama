e2a338a7d4dc4ec5b2783f4e3d715b7a248b9f10
Kartik Null Cating-Subramanian <ksubrama@simplescientist.net>
2014-11-01 16:27:33 -0400

    Actually fixup prep so it works the first time :-P

--- ksubrama/prep.ps1
+++ ksubrama/prep.ps1
@@ -1,2 +1,2 @@
-dialyzer --build_plt --apps erts kernel stdlib crypto mnesia sasl common_test eunit ssl reltool -r .\deps\ej\ebin .\deps\jsx\ebin .\deps\ranch\ebin .\deps\cowboy\ebin .\deps\cowlib\ebin .\ebin
 .\build
+dialyzer --build_plt --apps erts kernel stdlib crypto mnesia sasl common_test eunit ssl reltool -r .\deps\ej\ebin .\deps\jsx\ebin .\deps\ranch\ebin .\deps\cowboy\ebin .\deps\cowlib\ebin .\ebin
